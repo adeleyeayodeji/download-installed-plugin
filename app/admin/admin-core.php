462a9cfe064e36142865f2c3c806a27dc6ac4f6d
adeleyeayodeji <biodunhi@gmail.com>
2024-10-22 08:39:40 +0100

    Added main files

--- app/admin/admin-core.php
+++ app/admin/admin-core.php
@@ -22,5 +22,36 @@ class Admin_Core extends Base
 	 *
 	 * @return void
 	 */
-	public function init() {}
+	public function init()
+	{
+		//add column to plugins page
+		add_filter('manage_plugins_columns', array($this, 'add_download_column'));
+		add_action('manage_plugins_custom_column', array($this, 'render_download_column'), 10, 2);
+	}
+
+	/**
+	 * Add download column to plugins page
+	 *
+	 * @param array $columns
+	 * @return array
+	 */
+	public function add_download_column($columns)
+	{
+		$columns['download-installed-plugin'] = __('Download');
+		return $columns;
+	}
+
+	/**
+	 * Render download column
+	 *
+	 * @param string $column_name
+	 * @param int $plugin_file
+	 * @return void
+	 */
+	public function render_download_column($column_name, $plugin_file)
+	{
+		if ($column_name === 'download-installed-plugin') {
+			echo '<a href="' . esc_url(admin_url('plugins.php?action=download-installed-plugin&plugin=' . $plugin_file)) . '" class="button button-primary download-installed-plugin">' . __('Download') . '</a>';
+		}
+	}
 }
--- download-installed-plugin.php
+++ download-installed-plugin.php
@@ -1,10 +1,11 @@
 <?php
+
 /**
  * Plugin Name:     Download Installed Plugin
- * Plugin URI:      PLUGIN SITE HERE
- * Description:     PLUGIN DESCRIPTION HERE
- * Author:          YOUR NAME HERE
- * Author URI:      YOUR SITE HERE
+ * Plugin URI:      https://www.biggidroid.com/
+ * Description:     Download Installed Plugin
+ * Author:          Biggidroid
+ * Author URI:      https://www.biggidroid.com/
  * Text Domain:     download-installed-plugin
  * Domain Path:     /languages
  * Version:         0.1.0
@@ -12,4 +13,13 @@
  * @package         Download_Installed_Plugin
  */
 
-// Your code starts here.
+//check for security
+if (!defined('ABSPATH')) {
+	exit("You can't access file directly");
+}
+
+//load the plugin
+require_once __DIR__ . '/vendor/autoload.php';
+
+//init the plugin
+Download_Installed_Plugin\Loader::instance();
